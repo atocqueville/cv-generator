0b484878bac268a87bcd975857c899804ae2a63d
atocqueville <alex.detocqueville@gmail.com>
2020-07-30 00:47:02 +0200

    max height 16825 precisely

--- src/data/document.js
+++ src/data/document.js
@@ -94,7 +94,7 @@ export class DocumentCreator {
 
     const tableRow = new TableRow({
       height: {
-        height: 16800,
+        height: 16825,
         rule: HeightRule.EXACT,
         cantSplit: true,
       },
@@ -337,32 +337,32 @@ export class DocumentCreator {
 
   getMonthFromInt(value) {
     switch (value) {
-      case 1:
-        return "Jan";
-      case 2:
-        return "Feb";
-      case 3:
-        return "Mar";
-      case 4:
-        return "Apr";
-      case 5:
-        return "May";
-      case 6:
-        return "Jun";
-      case 7:
-        return "Jul";
-      case 8:
-        return "Aug";
-      case 9:
-        return "Sept";
-      case 10:
-        return "Oct";
-      case 11:
-        return "Nov";
-      case 12:
-        return "Dec";
-      default:
-        return "N/A";
+    case 1:
+      return "Jan";
+    case 2:
+      return "Feb";
+    case 3:
+      return "Mar";
+    case 4:
+      return "Apr";
+    case 5:
+      return "May";
+    case 6:
+      return "Jun";
+    case 7:
+      return "Jul";
+    case 8:
+      return "Aug";
+    case 9:
+      return "Sept";
+    case 10:
+      return "Oct";
+    case 11:
+      return "Nov";
+    case 12:
+      return "Dec";
+    default:
+      return "N/A";
     }
   }
 }
